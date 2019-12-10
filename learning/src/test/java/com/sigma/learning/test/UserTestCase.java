b2f52925141afff22fc6e3eb7474427a958cfe16
Murty <klmurty2019@gmail.com>
2019-12-10 09:35:34 +0530

    Testcase is added

---
+++ learning/src/test/java/com/sigma/learning/test/UserTestCase.java
@@ -0,0 +1,13 @@
+package com.sigma.learning.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class UserTestCase {
+
+	@Test
+	public void doTest() {
+		assertTrue(true);
+	}
+}
